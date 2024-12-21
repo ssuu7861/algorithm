ce09485853275f65d96f792d87cee3bea75778c0
ssss._.uu <ssuu7861@gmail.com>
2024-12-21 06:54:10 +0000

    53. Balanced Binary Tree

---
+++ java-algorithm-interview/src/balanced_binary_tree_53_110/BalancedBinaryTree.java
@@ -0,0 +1,29 @@
+public class BalancedBinaryTree {
+
+    public boolean isBalanced(TreeNode root) {
+        return dfs(root) != -1;
+    }
+    public int dfs(TreeNode root) {
+        if(root == null) return 0;
+        
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+        
+        if(left == -1 || right == -1 || Math.abs(left - right) > 1) return -1;
+
+        return Math.max(left, right) + 1;
+    }
+    
+}
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
