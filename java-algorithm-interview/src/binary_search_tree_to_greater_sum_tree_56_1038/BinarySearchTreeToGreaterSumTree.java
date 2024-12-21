a03f8a0928a386fc16f28a0992f2936094b375f0
ssss._.uu <ssuu7861@gmail.com>
2024-12-21 07:50:57 +0000

    56. Binary Search Tree to Greater Sum Tree

---
+++ java-algorithm-interview/src/binary_search_tree_to_greater_sum_tree_56_1038/BinarySearchTreeToGreaterSumTree.java
@@ -0,0 +1,31 @@
+package binary_search_tree_to_greater_sum_tree_56_1038;
+
+
+public class BinarySearchTreeToGreaterSumTree {
+
+    int sum = 0;
+
+    public TreeNode bstToGst(TreeNode root) {
+        if(root != null) {
+            bstToGst(root.right);
+            sum += root.val;
+            root.val = sum;
+            bstToGst(root.left);
+        }
+        return root;
+    }
+
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
