1509beeff37ece71653080f9556248a3e4c5b72f
ssss._.uu <ssuu7861@gmail.com>
2024-12-29 07:38:35 +0000

    68. Valid Anagram

---
+++ java-algorithm-interview/src/valid_anagram_68_242/ValidAnagram.java
@@ -0,0 +1,20 @@
+package valid_anagram_68_242;
+
+import java.util.Arrays;
+
+public class ValidAnagram {
+
+    public static void main(String[] args) {
+        String s = "anagram", t = "nagaram";
+        System.out.println(isAnagram(s, t));
+    }
+
+    public static boolean isAnagram(String s, String t) {
+        char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
+        Arrays.sort(sChars);
+        Arrays.sort(tChars);
+
+        return new String(sChars).equals(new String(tChars));
+    }
+}
