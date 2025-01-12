4bd17bbd1843c5e823f02cd8127b64a84a4a649c
ssss._.uu <ssuu7861@gmail.com>
2025-01-12 12:17:08 +0000

    edit> 95. Thievery

--- java-algorithm-interview/src/programmers_95_42897/Thievery.java
+++ java-algorithm-interview/src/programmers_95_42897/Thievery.java
@@ -5,16 +5,16 @@ import java.util.Arrays;
 public class Thievery {
 
     public static void main(String[] args) {
-        int[] money = {91,90,5,7,5,7};
+        int[] money = {10, 2, 2, 100, 2};
         System.out.println(thievery(money));
     }
-
     public static int thievery(int[] money) {
-        if(money.length <= 3) return Arrays.stream(money).max().getAsInt();
+        if(money.length == 3) return Arrays.stream(money).max().getAsInt();
 
         int[][] dp = new int[2][money.length];
 
         dp[0][0] = money[0];
+        dp[0][1] = Math.max(money[0], money[1]); // dp[1][1] 은 money[1]을 무조건 뽑으니까 dp[0][] 라인은 둘 중 큰걸 골라아한다!
         dp[1][1] = money[1];
         
         for(int i = 2 ; i < money.length ; i++) {
