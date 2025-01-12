6d575710e08974a4a2d18d4a4a06b4115f9f4bcf
ssss._.uu <ssuu7861@gmail.com>
2025-01-12 10:24:18 +0000

    95. Thievery

---
+++ java-algorithm-interview/src/programmers_95_42897/Thievery.java
@@ -0,0 +1,27 @@
+package programmers_95_42897;
+
+import java.util.Arrays;
+
+public class Thievery {
+
+    public static void main(String[] args) {
+        int[] money = {91,90,5,7,5,7};
+        System.out.println(thievery(money));
+    }
+
+    public static int thievery(int[] money) {
+        if(money.length <= 3) return Arrays.stream(money).max().getAsInt();
+
+        int[][] dp = new int[2][money.length];
+
+        dp[0][0] = money[0];
+        dp[1][1] = money[1];
+        
+        for(int i = 2 ; i < money.length ; i++) {
+            dp[0][i] = Math.max(dp[0][i - 1], dp[0][i - 2] + money[i]);
+            dp[1][i] = Math.max(dp[1][i - 1], dp[1][i - 2] + money[i]);
+        }
+
+        return Math.max(dp[0][money.length - 2], dp[1][money.length - 1]);
+    }
+}
