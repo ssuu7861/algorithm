1b7f98f7f6af50065accac75999217ad8f596952
ssss._.uu <ssuu7861@gmail.com>
2025-01-31 12:19:38 +0000

    1. 숫자의 합 구하기(11720)

---
+++ algorithm-coding-test/BOJ_1_11720/Main.java
@@ -0,0 +1,15 @@
+package BOJ_1_11720;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+public class Main {
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int sum = 0;
+        for(char c : br.readLine().toCharArray()) sum += c - '0';
+        System.out.println(sum);
+    }
+}
