a277b2f187fd68f8227c70d4dbd904168b8cd946
ssss._.uu <ssuu7861@gmail.com>
2025-02-01 12:11:06 +0000

    4. 구간 합 구하기 5(11660)

---
+++ algorithm-coding-test/BOJ_4_11660/Main.java
@@ -0,0 +1,34 @@
+package BOJ_4_11660;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokenizer = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(tokenizer.nextToken());
+        int m = Integer.parseInt(tokenizer.nextToken());
+        int[][] sum = new int[n + 1][n + 1];
+
+        for(int i = 1 ; i <= n ; i++) {
+            tokenizer = new StringTokenizer(br.readLine());
+            for(int j = 1 ; j <= n ; j++) {
+                sum[i][j] = sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1] + Integer.parseInt(tokenizer.nextToken());
+            }
+        }
+        
+        for(int i = 0 ; i < m ; i++) {
+            tokenizer = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(tokenizer.nextToken());
+            int y1 = Integer.parseInt(tokenizer.nextToken());
+            int x2 = Integer.parseInt(tokenizer.nextToken());
+            int y2 = Integer.parseInt(tokenizer.nextToken());
+
+            System.out.println(sum[x2][y2] - sum[x1 - 1][y2] - sum[x2][y1 - 1] + sum[x1 - 1][y1 - 1]);
+        }
+    }
+}
