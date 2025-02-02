ec6f1f1aba8672a035aac15ea64bb984373a9f14
ssss._.uu <ssuu7861@gmail.com>
2025-02-02 07:48:06 +0000

    7. 주몽(1940)

---
+++ algorithm-coding-test/BOJ_7_1940/Main.java
@@ -0,0 +1,33 @@
+package BOJ_7_1940;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+        StringTokenizer tokenizer = new StringTokenizer(br.readLine());
+        for(int i = 0 ; i < n ; i++) arr[i] = Integer.parseInt(tokenizer.nextToken());
+        Arrays.sort(arr);
+        int i = 0, j = n - 1;
+        int result = 0;
+        while (i < j) {
+            int sum = arr[i] + arr[j];
+            if(sum == m) {
+                result++;
+                i++;
+                j--;
+            } else if(sum > m) j--;
+            else i++;
+        }
+        System.out.println(result);
+    }
+}
