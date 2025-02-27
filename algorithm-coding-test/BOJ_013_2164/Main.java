a23dddfce487696ae56ac958e55df46fcfdc1809
ssss._.uu <ssuu7861@gmail.com>
2025-02-27 11:07:59 +0000

    13. 카드2(2164)

---
+++ algorithm-coding-test/BOJ_013_2164/Main.java
@@ -0,0 +1,22 @@
+package BOJ_013_2164;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        LinkedList<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= n; i++) {
+            queue.add(i);
+        }
+        while (queue.size() > 1) {
+            queue.poll();
+            queue.add(queue.poll());
+        }
+        System.out.println(queue.poll());
+    }
+}
